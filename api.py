54de65a0a5d8d7284a5e3e7840a2bfb8b93efcf4
Diego <diegodutraramos@gmail.com>
2026-04-19 14:55:21 -0300

    Corrige CORS da API para o app Tauri

--- api.py
+++ api.py
@@ -41,6 +41,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "tauri://localhost",
+        "http://tauri.localhost",
+        "https://tauri.localhost",
     ],
     allow_credentials=True,
     allow_methods=["*"],
